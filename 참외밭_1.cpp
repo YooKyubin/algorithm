6eed0bf17700c1f2ed30158d486d2cfd665c7007
YooKyubin <binde73@naver.com>
2022-10-03 21:27:49 +0900

    참외밭의 다른 풀이

---
+++ 참외밭_1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int k;
+int dx[7] = {0};
+int y[7] = {0};
+int direct[5][2] = {
+    {0, 0},
+    {1, 0},
+    {-1,0},
+    {0,-1},
+    {0, 1}
+};
+int area = 0;
+
+int main(){
+    cin >> k;
+    for(int i=1; i < 7; i++){
+        int dir, len;
+        cin >> dir >> len;
+
+        // x의 변화량 만을 저장
+        dx[i]= direct[dir][0] * len;
+        // y의 좌표를 저장 (이전 좌표값 + 변화량)
+        y[i] = y[i-1] + direct[dir][1] * len;
+
+    }
+    for(int i=1; i<7; i++){
+        // x변화량 * y좌표값
+        area += -dx[i] * y[i];
+    }
+
+    cout << area * k << endl;
+
+    return 0;
+}
