55c578c14e5a84d59168fbe5dddb0abeb600f954
YooKyubin <binde73@naver.com>
2022-10-08 17:33:11 +0900

    구조체를 사용하여 큐에 넣을 때 코드가 조금 달라졌다.(vector를 사용하지 않음)

--- 이모티콘.cpp
+++ 이모티콘.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<queue>
-#include<vector>
 
 #define MAX 1001 // 2 <= s  < 1000
 
@@ -12,14 +11,20 @@ int v;
 int clip;
 int t;
 
+struct factor{
+    int q_v;
+    int q_clip;
+    int q_t;
+};
+
 void bfs(){
-    queue<vector<int>> q;
-    q.push(vector<int> {1,0,0}); // 화면, 클립보드
+    queue<factor> q;
+    q.push({1,0,0}); // 화면, 클립보드, 시간
     visited[1][0] = true; //화면, 클립보드
     while (!q.empty()){
-        v = q.front()[0];
-        clip = q.front()[1];
-        t = q.front()[2];
+        v = q.front().q_v;
+        clip = q.front().q_clip;
+        t = q.front().q_t;
 
         if(v == s){
             return;
@@ -30,17 +35,17 @@ void bfs(){
         // 화면의 이모티콘 하나를 삭제한다.
         if(v-1 > 0 && !visited[v-1][clip]){
             visited[v-1][clip] = true;
-            q.push(vector<int> {v-1, clip, t + 1});
+            q.push({v-1, clip, t + 1});
         }
         // 클립보드의 모든 이모티콘을 화면에 붙여넣기 한다.
         if(v+clip < MAX && clip > 0 && !visited[v+clip][clip]){
             visited[v+clip][clip] = true;
-            q.push(vector<int> {v+clip, clip, t + 1});
+            q.push({v+clip, clip, t + 1});
         }
         // 화면읜 있는 이모티콘을 모두 복사하여 클립보드에 저장한다.
         if(!visited[v][v]){
             visited[v][v] = true;
-            q.push(vector<int> {v, v, t + 1});
+            q.push({v, v, t + 1});
         }
     }
 }
