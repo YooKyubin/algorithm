5e65f9f256a215c3fa75d2cd6f9ff787617c6fd2
YooKyubin <binde73@naver.com>
2022-10-08 17:17:12 +0900

    20221008

---
+++ 이모티콘.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+#define MAX 1001 // 2 <= s  < 1000
+
+using namespace std;
+
+int s;
+int visited[MAX][MAX] = {false};
+int v;
+int clip;
+int t[MAX][MAX];
+
+void bfs(){
+    queue<vector<int>> q;
+    q.push(vector<int> {1,0}); // 화면, 클립보드
+    visited[1][0] = true; //화면, 클립보드
+    t[1][0] = 0; //화면, 클립보드
+    while (!q.empty()){
+        v = q.front()[0];
+        clip = q.front()[1];
+
+        if(v == s){
+            return;
+        }
+
+        q.pop();
+        
+        // 화면의 이모티콘 하나를 삭제한다.
+        if(v-1 > 0 && !visited[v-1][clip]){
+            visited[v-1][clip] = true;
+            t[v-1][clip] = t[v][clip] + 1;
+            q.push(vector<int> {v-1, clip});
+        }
+        // 클립보드의 모든 이모티콘을 화면에 붙여넣기 한다.
+        if(v+clip < MAX && clip > 0 && !visited[v+clip][clip]){
+            visited[v+clip][clip] = true;
+            t[v+clip][clip] = t[v][clip] + 1;
+            q.push(vector<int> {v+clip, clip});
+        }
+        // 화면읜 있는 이모티콘을 모두 복사하여 클립보드에 저장한다.
+        if(!visited[v][v]){
+            visited[v][v] = true;
+            t[v][v] = t[v][clip] + 1;
+            q.push(vector<int> {v, v});
+        }
+    }
+}
+
+int main(){
+    cin >> s;
+    bfs();
+    cout << t[v][clip] << endl;
+    return 0;
+}
