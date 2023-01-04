ab1ef74dbc2c13e815c4b2365289331acfddb956
YooKyubin <binde73@naver.com>
2023-01-05 00:21:50 +0900

    수정

--- 주식가격.cpp
+++ 주식가격.cpp
@@ -5,32 +5,23 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+    vector<int> answer(size(prices));
     vector<int> stack;
-    answer.assign(size(prices), 0);
+
+    for (int i=0; i < size(answer); i++){
+        answer[i] = size(answer) - i -1;
+    }
 
     for(int i=0; i < size(prices); i++){
-        if(stack.empty() || stack.back() < prices[i]){
-            cout << "stack_push: " << prices[i] << endl;
-            stack.push_back(prices[i]);
+        if(stack.empty() || prices[stack.back()] < prices[i]){
+            stack.push_back(i);
         }
         else{
-            cout << "pop index: " << i << endl;
-            int t = 1;
-            while (!stack.empty() && stack.back() > prices[i]){
-                if (answer[i-t] == 0){
-                    answer[i-t] = t;
-                    cout << "answr index: " << i-t << ", pop element: " << stack.back() << endl;
-                    stack.pop_back();
-                }
-                t = t+1;
+            while (!stack.empty() && prices[stack.back()] > prices[i]){
+                answer[stack.back()] = i - stack.back();
+                stack.pop_back();
             }
-            stack.push_back(prices[i]);
-        }
-    }
-    for (int i=0; i < size(answer); i++){
-        if (answer[i] == 0){
-            answer[i] = size(answer) - i -1;
+            stack.push_back(i);
         }
     }
     return answer;
