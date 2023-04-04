222bbf1f68805a68b425b8fee0492def658595a0
YooKyubin <binde73@naver.com>
2023-04-04 23:33:56 +0900

    살짝 변경

--- 1로_만들기.cpp
+++ 1로_만들기.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <vector>
 
 using namespace std;
 
-uint32_t memo[1000001];
+vector<int> memo(1000001, 0);
 
-uint32_t dp(uint32_t x){
+int dp(int x){
     // cout << x << endl;
     if (x == 1) return 0;
     else if(memo[x] != 0) return memo[x];
     else{
-        uint32_t a = -1;
-        uint32_t b = -1;
-        uint32_t c = -1;
-        if (x % 2 == 0) a = dp(x / 2);
-        if (x % 3 == 0) b = dp(x / 3);
-        c = dp(x-1);      
-        return memo[x] = min(a, min(b, c)) + 1;
+        memo[x] = dp(x-1) + 1;      
+        if (x % 2 == 0) memo[x] = min(memo[x], dp(x / 2)+1);
+        if (x % 3 == 0) memo[x] = min(memo[x], dp(x / 3)+1);
+        return memo[x];
     }
 }
 
 int main(){
-    uint32_t x;
+    int x;
     cin >> x;
-    memset(memo, 0, sizeof(uint32_t) * (x+1));
     cout << dp(x) << endl;
     return 0;
 }
