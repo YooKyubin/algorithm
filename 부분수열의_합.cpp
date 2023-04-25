1709a58109ea23bc0246cc138a01b679b5cf411b
YooKyubin <binde73@naver.com>
2023-04-26 03:44:19 +0900

    부분수열의 합 다른 풀이

--- 부분수열의_합.cpp
+++ 부분수열의_합.cpp
@@ -5,26 +5,29 @@ using namespace std;
 
 int n, s;
 vector<int> seq;
+int answer = 0;
+
+void dfs(int depth, int sum){
+
+    if ( depth == n ) return;
+    if ( sum + seq[depth] == s ) answer++;
+
+    dfs(depth+1, sum+seq[depth]);
+    dfs(depth+1, sum);
+}
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
-
-    int answer = 0;
 
     cin >> n >> s;
     seq.reserve(n);
     for (int i=0; i<n; i++){
         cin >> seq[i];
     }
-    for (int i=1; i< (1<<n); i++){
-        int number = 0;
-        for(int j=0; j < n; j++){
-            if ( i & 1<<j ) number += seq[j];
-        }
-        if (number == s) answer++;
-    }
 
+    dfs(0, 0);
+    
     cout << answer << "\n";
     return 0;
 }
