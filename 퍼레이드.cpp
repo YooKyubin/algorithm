b61cca3390cdea53ca42368162c13869153f272c
Yoo kyubin <binde73@naver.com>
2023-06-07 18:31:20 +0900

    퍼레이드 union find 활용

--- 퍼레이드.cpp
+++ 퍼레이드.cpp
@@ -4,14 +4,30 @@
 using namespace std;
 
 int v, e;
-vector<vector<int>> graph;
-vector<int> visited;
+vector<int> edgeCount;
+vector<int> head;
+vector<int> depth;
 
-void dfs(int v) {
-    visited[v] = true;
-    for (int next : graph[v]){
-        if (visited[next]) continue;
-        dfs(next);
+int find(int x){
+    if (head[x] == x) return x;
+    else return head[x] = find(head[x]);
+}
+
+void Union(int x, int y){
+    x = find(x);
+    y = find(y);
+
+    if (x == y) return;
+
+    if (depth[x] < depth[y]){
+        head[x] = y;
+    }
+    else if (depth[x] > depth[y]){
+        head[y] = x;
+    }
+    else {
+        head[x] = y;
+        depth[y]++;
     }
 }
 
@@ -20,32 +36,30 @@ int main () {
     ios_base::sync_with_stdio(false);
 
     cin >> v >> e;
-    graph.assign(v+1, vector<int>());
-    visited.assign(v+1, false);
+    edgeCount.assign(v+1, 0);
+    head.assign(v+1, 0);
+    for (int i=0; i<v+1; i++) head[i] = i;
+    depth.assign(v+1, 0);
 
     for (int i=0; i < e; i++){
         int a, b;
         cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        edgeCount[a]++;
+        edgeCount[b]++;
+        Union(a, b);
     }
-
-    dfs(1);
-    bool isSingle = true;
-    for (int i=1; i < v+1; i++){
-        if (!visited[i]) {
-            isSingle = false;
-            break;
-        }
-    }
-
+    
     string answer = "NO";
+    bool isSingle = true;
+    for (int i=1; i < v; i++){
+        if (find(i) != find(i+1)) isSingle = false;
+    }
+
     if (isSingle){
         int oddCount = 0;
         for (int i=1; i < v+1; i++){
-            if (graph[i].size() % 2 != 0) oddCount++;
+            if (edgeCount[i] % 2 != 0) oddCount++;
         }
-
         if (oddCount == 0 || oddCount == 2) answer = "YES";
     }
 
