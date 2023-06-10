9ad2743013649eb23ea795ba1cd72952db19cd83
Yoo kyubin <binde73@naver.com>
2023-06-10 20:55:20 +0900

    함수 포인터 사용

--- 배열_돌리기6.cpp
+++ 배열_돌리기6.cpp
@@ -18,8 +18,6 @@ offsetY = j / 2**1;
 #include <iostream>
 #include <vector>
 
-#define Pos pair<int, int>
-
 using namespace std;
 
 int N, R, n;
@@ -147,36 +145,34 @@ vector<vector<int>> func8(int l){
 }
 
 
-
 int main () {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
     cin >> N >> R;
     n = pow(2, N);
-    // cout << n << endl;
     table.assign(n, vector<int>(n, 0));
-    // vector<vector<int>> temp(n, vector<int>(n));
     for (int i=0; i < n; i++){
          for (int j=0; j < n; j++){
             cin >> table[i][j];
         }
     }
+
+    vector<vector<int>> (*fp[8])(int);
+    fp[0] = func1;
+    fp[1] = func2;
+    fp[2] = func3;
+    fp[3] = func4;
+    fp[4] = func5;
+    fp[5] = func6;
+    fp[6] = func7;
+    fp[7] = func8;
     
     for (int i=0; i < R; i++){
         int k, l;
         cin >> k >> l;
 
-        switch(k){
-            case 1: table = func1(l); break;
-            case 2: table = func2(l); break;
-            case 3: table = func3(l); break;
-            case 4: table = func4(l); break;
-            case 5: table = func5(l); break;
-            case 6: table = func6(l); break;
-            case 7: table = func7(l); break;
-            case 8: table = func8(l); break;
-        }
+        table = fp[k-1](l);
     }
     printTable();
     return 0;
