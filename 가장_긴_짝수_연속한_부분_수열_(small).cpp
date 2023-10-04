8c657fb2c1922582e664bfad8717c3e38ed919e7
Yoo kyubin <binde73@naver.com>
2023-10-04 19:49:36 +0900

    2023 10 04 가장 긴 짝수 연속한 부분 수열(small) 깔끔

--- 가장_긴_짝수_연속한_부분_수열_(small).cpp
+++ 가장_긴_짝수_연속한_부분_수열_(small).cpp
@@ -1,67 +1,39 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int n, k;
-vector<pair<bool, int>> odd;
-vector<int> input;
+bool odd[50000];
 
 int max(int a, int b) { return a > b ? a : b; }
 
 int main()
 {
     cin >> n >> k;
-
-    input.resize(n);
     for (int i=0; i<n; ++i)
     {
-        cin >> input[i];
+        int num;
+        cin >> num;
+        odd[i] = num % 2;
     }
-
-    bool pre = input[0] % 2;
-    int  cnt = 0;
-    for (int i=0; i<n; ++i)
-    {
-        if (pre == input[i] % 2)
-        {
-            ++cnt;
-        }
-        else
-        {
-            pair<bool, int> temp;
-            temp.first = pre;
-            temp.second = cnt;
-            odd.push_back(temp);
-
-            cnt = 1;
-            pre = input[i] % 2;
-        }
-    }
-    odd.push_back({pre, cnt});
 
     int oddCnt = 0;
-    int evenCnt = 0;
     int maxVal = 0;
     int right = 0;
     int left = 0;
-    while (right < odd.size())
+    while (right < n)
     {
-        if (odd[right].first)
-            oddCnt += odd[right].second;
-        else
-            evenCnt += odd[right].second;
+        if (odd[right])
+            ++oddCnt;
         
         while (oddCnt > k)
         {
-            if (odd[left].first)
-                oddCnt -= odd[left].second;
-            else
-                evenCnt -= odd[left].second;
+            if (odd[left])
+                --oddCnt;
             ++left;
         }
         
-        maxVal = max(maxVal, evenCnt);
+        maxVal = max(maxVal, right - left - oddCnt + 1);
         ++right;
     }
     
