14e475c6bbc5800b58e5d67b35bebc3c0fdb4cc6
Yookyubin <binde73@naver.com>
2025-11-01 19:06:27 +0900

    가장 긴 증가하는 부분 수열 5

--- 가장_긴_증가하는_부분_수열_5.cpp
+++ 가장_긴_증가하는_부분_수열_5.cpp
@@ -89,3 +89,86 @@ int main()
     }
     return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    freopen("../../input.txt", "r", stdin);
+
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> LIS;
+    LIS.reserve(n);
+    LIS.push_back(arr.front());
+    
+    vector<int> tracking(n, -1);
+    tracking[0] = 0;
+
+    for (int i = 1; i < n; ++i)
+    {
+        int curr = arr[i];
+        if (LIS.back() < curr)
+        {
+            tracking[i] = (int)LIS.size();
+            LIS.push_back(curr);
+        }
+        else
+        {
+            int low = -1;
+            int high = (int)LIS.size();
+            
+            while (low + 1 < high)
+            {
+                int mid = ( low + high ) >> 1;
+                
+                if ( curr <= LIS[mid] )
+                {
+                    high = mid; 
+                }
+                else
+                {
+                    low = mid;
+                }
+            }
+            LIS[high] = curr;
+            tracking[i] = high;
+        }
+    }
+
+    vector<int> answer;
+    int lastIndex = (int)LIS.size()-1;
+    for (int i = n-1; i >= 0; --i)
+    {
+        int id = tracking[i];
+
+        if (id == lastIndex)
+        {
+            answer.push_back(arr[i]);
+            --lastIndex;
+        }
+    }
+
+    cout << answer.size() << endl;
+    for (int i = (int)LIS.size() -1; i >= 0; --i)
+    {
+        cout << answer[i] << " ";
+    }
+    cout << endl;
+        
+    return 0;
+}
