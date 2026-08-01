827efce2432a5328aa80c6e92dbf7802a47734f7
YooKyubin <binde73@naver.com>
2026-08-01 17:19:34 +0900

    쉽게 프린트 할 수 있는 debug::print 함수 작성

---
+++ debug.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include <string_view>
+#include <utility>
+
+namespace debug
+{
+	inline std::string_view Trim(std::string_view name)
+	{
+		while (!name.empty() && name.front() == ' ')
+		{
+			name.remove_prefix(1);
+		}
+
+		while (!name.empty() && name.back() == ' ')
+		{
+			name.remove_suffix(1);
+		}
+
+		return name;
+	}
+
+	template <typename T, typename... Rest>
+	void Print(std::string_view names, T&& value, Rest&&... rest)
+	{
+		const std::size_t comma = names.find(',');
+		const std::string_view name = Trim(names.substr(0, comma));
+
+		std::cerr << name << ": " << std::forward<T>(value);
+
+		if constexpr (sizeof...(Rest) > 0)
+		{
+			std::cerr << ", ";
+			Print(names.substr(comma + 1), std::forward<Rest>(rest)...);
+		}
+		else
+		{
+			std::cerr << '\n';
+		}
+	}
+}
+
+#define DBG(...) ::debug::Print(#__VA_ARGS__, __VA_ARGS__)
+
